babe91edfe675abf63c7b8effb77c1c1cc84042d
Nicholas Myers <32116122+NicholasMy@users.noreply.github.com>
2023-06-20 10:25:53 -0400

    General Quasar configuration and cleanup, create a dev proxy Docker config

---
+++ dev-proxy/README.md
@@ -0,0 +1,7 @@
+This is a quick proxy setup for development. It'll proxy /portal to the front end and /portal/api to the backend,
+assuming both are running locally on your host.
+
+On your host, Quasar should be serving the development frontend on port 9000
+The backend should be running on port 5057
+
+In your browser, connect to http://localhost:80/portal.
--- frontend/autolab-self-service/quasar.config.js
+++ frontend/autolab-self-service/quasar.config.js
@@ -30,8 +30,8 @@ module.exports = configure(function (/* ctx */) {
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
     boot: [
-      
-      
+
+
     ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
@@ -67,7 +67,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/portal',
       // analyze: true,
       // env: {},
       // rawDefine: {}
@@ -79,7 +79,7 @@ module.exports = configure(function (/* ctx */) {
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
 
-      
+
       // vitePlugins: [
       //   [ 'package-name', { ..options.. } ]
       // ]
@@ -88,7 +88,7 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
       // https: true
-      open: true // opens browser window automatically
+      open: false // opens browser window automatically
     },
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
