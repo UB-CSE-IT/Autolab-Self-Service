d11350ced1ecb6d6878e444e589b464127ebf489
Nicholas Myers <32116122+NicholasMy@users.noreply.github.com>
2023-07-19 15:29:57 -0400

    Add alternative view to Tango statistics page

--- frontend/autolab-self-service/src/pages/TangoStatsPage.vue
+++ frontend/autolab-self-service/src/pages/TangoStatsPage.vue
@@ -4,6 +4,8 @@
     <div class="button-row q-mb-lg">
       <q-btn icon="west" label="Back to portal home" color="primary" to="/"/>
       <q-btn icon="refresh" label="Refresh" color="primary" @click="updateTangoStats"/>
+      <q-btn icon="visibility" label="Toggle Alternative View" color="primary"
+             @click="state.alternativeView = !state.alternativeView"/>
     </div>
 
     <div v-if="state.loading">
@@ -16,12 +18,42 @@
       <p v-if="state.success">(Showing previously fetched data below)</p>
     </div>
     <template v-if="state.success">
-      <div v-for="point in state.data" :key="point.seconds">
-        <div class="histogram-entry">
-          <div class="bar" :style="{width: `${point.percent}%`}"/>
-          <p>{{ point.sentence }}</p>
+      <template v-if="state.alternativeView">
+        <div class="row">
+          <div>
+            <div class="timeframe histogram-entry">
+              <p><b>Timeframe</b></p>
+            </div>
+          </div>
+          <div class="col-grow">
+            <div class="histogram-entry">
+              <p><b>Submissions within timeframe</b></p>
+            </div>
+          </div>
         </div>
-      </div>
+        <div class="row" v-for="point in state.data" :key="point.seconds">
+          <div>
+            <div class="timeframe histogram-entry">
+              <p>{{ point.timeframe }}</p>
+            </div>
+          </div>
+          <div class="col-grow">
+            <div class="histogram-entry alt">
+              <div class="bar flex" :style="{width: `${point.percent}%`}">
+                <p style="text-align: right; width: 100%; margin-right: 6px;">{{ point.count }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </template>
+      <template v-else>
+        <div v-for="point in state.data" :key="point.seconds">
+          <div class="histogram-entry">
+            <div class="bar" :style="{width: `${point.percent}%`}"/>
+            <p>{{ point.sentence }}</p>
+          </div>
+        </div>
+      </template>
     </template>
 
     <div style="height: 100px;"></div>
@@ -39,7 +71,8 @@ const state = reactive({
   error: false, // This will be true if the last refresh request failed
   success: false, // This will remain true once there is data, even if a later refresh request fails
   message: '', // Use to show errors
-  data: null as TangoHistogramPoint[] | null
+  data: null as TangoHistogramPoint[] | null,
+  alternativeView: false,
 })
 
 function updateTangoStats() {
@@ -86,6 +119,11 @@ updateTangoStats()
 .histogram-entry .bar {
   background-color: #8fb7ff;
   border-radius: 10px;
+}
+
+.timeframe {
+  width: 140px;
+  text-align: right;
 }
 
 </style>
