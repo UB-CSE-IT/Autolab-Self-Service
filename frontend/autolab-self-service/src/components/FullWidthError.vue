65b8471c3a38b76876d85fcd2af8aeb682d1e36d
Nicholas Myers <32116122+NicholasMy@users.noreply.github.com>
2023-08-16 12:49:08 -0400

    Standardize styling in FullWidthError and FullWidthLoading

--- frontend/autolab-self-service/src/components/FullWidthError.vue
+++ frontend/autolab-self-service/src/components/FullWidthError.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="column items-center q-ma-lg error-box">
+  <div class="column items-center q-my-lg error-box">
     <q-icon class="q-mb-lg" name="error" color="negative" size="100px"/>
     <slot/>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@import 'src/css/quasar.variables.scss';
+
 .error-box {
   background-color: #ffebee;
-  border: 1px solid #ffcdd2;
+  border: 1px solid $negative;
   border-radius: 4px;
   padding: 16px;
 }
--- frontend/autolab-self-service/src/components/FullWidthLoading.vue
+++ frontend/autolab-self-service/src/components/FullWidthLoading.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="column items-center q-ma-lg loading-box">
+  <div class="column items-center q-my-lg loading-box">
     <q-spinner-hourglass class="q-mb-lg" color="primary" size="100px"/>
     <slot/>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@import 'src/css/quasar.variables.scss';
+
 .loading-box {
   background-color: #e3f2fd;
-  border: 1px solid #bbdefb;
+  border: 1px solid $primary;
   border-radius: 4px;
   padding: 16px;
 }
